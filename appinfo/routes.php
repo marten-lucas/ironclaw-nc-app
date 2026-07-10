8bc7eb638a20459ec3695ad401aa0d69283b8219
marten-lucas <be_smarten@yahoo.de>
2026-07-10 10:44:38 +0200

    fix(settings): use dedicated admin page route for stable post-save rendering

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -5,6 +5,11 @@ declare(strict_types=1);
 return [
 	'routes' => [
 		[
+			'name' => 'Settings#index',
+			'url' => '/settings/admin',
+			'verb' => 'GET',
+		],
+		[
 			'name' => 'Settings#save',
 			'url' => '/settings/admin/save',
 			'verb' => 'POST',
