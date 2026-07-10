95a34e9950f002ef4556d389ab211b493178cd6c
marten-lucas <be_smarten@yahoo.de>
2026-07-10 14:29:55 +0200

    fix(settings): use app-id section key for nc33 admin auth compatibility

--- lib/Settings/AdminSection.php
+++ lib/Settings/AdminSection.php
@@ -9,7 +9,7 @@ use OCP\Settings\IIconSection;
 
 class AdminSection implements IIconSection {
 	public function getID(): string {
-		return 'ironclaw-talk-bridge';
+		return Application::APP_ID;
 	}
 
 	public function getName(): string {
