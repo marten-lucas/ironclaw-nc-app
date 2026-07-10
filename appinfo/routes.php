ebfe682ff6b720a9b9c24db382b46e13cbe526b9
marten-lucas <be_smarten@yahoo.de>
2026-07-10 10:48:16 +0200

    fix(settings): revert dedicated route and keep stable admin redirect

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -5,11 +5,6 @@ declare(strict_types=1);
 return [
 	'routes' => [
 		[
-			'name' => 'Settings#index',
-			'url' => '/settings/admin',
-			'verb' => 'GET',
-		],
-		[
 			'name' => 'Settings#save',
 			'url' => '/settings/admin/save',
 			'verb' => 'POST',
--- lib/Controller/SettingsController.php
+++ lib/Controller/SettingsController.php
@@ -8,7 +8,6 @@ use OCA\IronclawTalkBridge\AppInfo\Application;
 use OCP\AppFramework\Controller;
 use OCP\AppFramework\Http\JSONResponse;
 use OCP\AppFramework\Http\RedirectResponse;
-use OCP\AppFramework\Http\TemplateResponse;
 use OCP\IConfig;
 use OCP\Http\Client\IClientService;
 use OCP\IRequest;
@@ -17,7 +16,6 @@ use OCP\IURLGenerator;
 
 class SettingsController extends Controller {
 	private const SETTINGS_ANCHOR = '#ironclaw-talk-bridge-admin-settings';
-	private const SETTINGS_ROUTE = 'ironclaw_talk_bridge.Settings.index';
 
 	public function __construct(
 		IRequest $request,
@@ -27,69 +25,6 @@ class SettingsController extends Controller {
 		private IURLGenerator $urlGenerator,
 	) {
 		parent::__construct(Application::APP_ID, $request);
-	}
-
-	public function index(string $ictb_status = '', string $ictb_msg = ''): TemplateResponse {
-		$secret = $this->config->getAppValue(Application::APP_ID, 'ironclaw_shared_secret', '');
-		$fakeUserId = $this->config->getAppValue(Application::APP_ID, 'fake_user_id', '');
-		$fakeUserName = $this->config->getAppValue(Application::APP_ID, 'mention_display_name', '');
-
-		$users = [];
-		try {
-			$foundUsers = [];
-			if (method_exists($this->userManager, 'searchDisplayName')) {
-				$foundUsers = $this->userManager->searchDisplayName('');
-			} elseif (method_exists($this->userManager, 'search')) {
-				$foundUsers = $this->userManager->search('');
-			}
-
-			foreach ($foundUsers as $user) {
-				$users[] = [
-					'uid' => $user->getUID(),
-					'displayName' => $user->getDisplayName(),
-				];
-			}
-		} catch (\Throwable $e) {
-			$users = [];
-		}
-
-		if ($fakeUserId !== '') {
-			$hasSelectedUser = false;
-			foreach ($users as $user) {
-				if ((string)$user['uid'] === (string)$fakeUserId) {
-					$hasSelectedUser = true;
-					break;
-				}
-			}
-
-			if (!$hasSelectedUser) {
-				$users[] = [
-					'uid' => $fakeUserId,
-					'displayName' => $fakeUserName !== '' ? $fakeUserName : $fakeUserId,
-				];
-			}
-		}
-
-		usort($users, static function (array $a, array $b): int {
-			return strcmp((string)$a['displayName'], (string)$b['displayName']);
-		});
-
-		return new TemplateResponse(Application::APP_ID, 'settings-admin', [
-			'values' => [
-				'enabled' => $this->config->getAppValue(Application::APP_ID, 'enabled', '0') === '1',
-				'strict_membership_resolver' => $this->config->getAppValue(Application::APP_ID, 'strict_membership_resolver', '1') === '1',
-				'ironclaw_inbound_url' => $this->config->getAppValue(Application::APP_ID, 'ironclaw_inbound_url', ''),
-				'fake_user_name' => $fakeUserName,
-				'fake_user_id' => $fakeUserId,
-				'room_allowlist_tokens' => $this->config->getAppValue(Application::APP_ID, 'room_allowlist_tokens', ''),
-				'dispatch_batch_size' => $this->config->getAppValue(Application::APP_ID, 'dispatch_batch_size', '50'),
-				'signature_tolerance_seconds' => $this->config->getAppValue(Application::APP_ID, 'signature_tolerance_seconds', '300'),
-			],
-			'users' => $users,
-			'uiStatus' => (string)$ictb_status,
-			'uiMessage' => (string)$ictb_msg,
-			'secretConfigured' => $secret !== '',
-		], '');
 	}
 
 	public function save(
@@ -105,7 +40,8 @@ class SettingsController extends Controller {
 		$trimmedUrl = trim($ironclaw_inbound_url);
 		$redirectNonce = (string)time();
 		if ($trimmedUrl === '' || filter_var($trimmedUrl, FILTER_VALIDATE_URL) === false) {
-			return new RedirectResponse($this->urlGenerator->linkToRoute(self::SETTINGS_ROUTE, [
+			return new RedirectResponse($this->urlGenerator->linkToRoute('settings.AdminSettings.index', [
+				'section' => 'server',
 				'ictb_status' => 'error',
 				'ictb_msg' => 'Bitte eine gueltige Ironclaw URL eintragen.',
 				'ictb_ts' => $redirectNonce,
@@ -126,7 +62,8 @@ class SettingsController extends Controller {
 		}
 
 		if ($uid === '' || $displayName === '') {
-			return new RedirectResponse($this->urlGenerator->linkToRoute(self::SETTINGS_ROUTE, [
+			return new RedirectResponse($this->urlGenerator->linkToRoute('settings.AdminSettings.index', [
+				'section' => 'server',
 				'ictb_status' => 'error',
 				'ictb_msg' => 'Bitte einen gueltigen Fake User aus der Liste waehlen.',
 				'ictb_ts' => $redirectNonce,
@@ -147,7 +84,8 @@ class SettingsController extends Controller {
 			$this->config->setAppValue(Application::APP_ID, 'ironclaw_shared_secret', $ironclaw_shared_secret);
 		}
 
-		return new RedirectResponse($this->urlGenerator->linkToRoute(self::SETTINGS_ROUTE, [
+		return new RedirectResponse($this->urlGenerator->linkToRoute('settings.AdminSettings.index', [
+			'section' => 'server',
 			'ictb_status' => 'success',
 			'ictb_msg' => 'Einstellungen gespeichert.',
 			'ictb_ts' => $redirectNonce,
